cdcacbc753baec5866bb21da0d2fb5f1891cc6a5
Mosaddeq Hossain <66480280+Mosaddeq107@users.noreply.github.com>
2023-11-14 19:40:58 -0500

    Update test_co2_functions.py

--- test_co2_functions.py
+++ test_co2_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from co2_analysis.py import calculate_frequency, calculate_peak_frequency, find_peak_frequency
+from co2_analysis import calculate_frequency, calculate_peak_frequency, find_peak_frequency
 
 def test_calculate_frequency():
     data = np.array([1, 2, 3, 4, 5])
